be81768f4a98df1c5dee1bfbb2c76fcf5328a768
Joylson <joylsont@gmail.com>
2018-04-01 23:05:55 -0300

    Inseriri projeto

---
+++ Crud_Entity/Crud_Entity/Context/ModelContext.cs
@@ -0,0 +1,22 @@
+﻿using Crud_Entity.Models;
+using System.Data.Entity;
+
+namespace Crud_Entity.Context
+{
+    public class ModelContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ModelContext() : base("name=ModelContext")
+        {
+        }
+
+        public DbSet<Produto> Produtoes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Crud_Entity/Crud_Entity/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Crud_Entity.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+        public int Qtde { get; set; }
+    }
+}
